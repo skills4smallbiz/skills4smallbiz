20fcba174af144f389d024db14fa1a58480f68fd
Lucien Gaitskell <lucieng@gaitskell.com>
2020-03-29 13:06:09 -0400

    Add upgraded filter business and volunteer pages

--- public/assets/js/dbload.js
+++ public/assets/js/dbload.js
@@ -1,21 +1,51 @@
-function loadSome(col, prefix, keys) {
-    var table = document.getElementById(prefix + "-table");
+function printData(qtype, prefix, fields, servlist){
+    var table = document.getElementById(prefix+'-table')
 
-    var db = firebase.firestore();
-
-    db.collection(col).get().then(function(snapshot) {
-    snapshot.forEach(function(doc) {
-        console.log(doc.data());
-        var row = table.insertRow(-1);
-        var cells = [];
-        keys.forEach(function(id, idx) {
-            cells.push(row.insertCell());
-            cells[idx].innerHTML = doc.data()[id]
-            cells[idx].id = prefix + '-' + id;
-        });
+    db.collection(qtype).where('services', 'array-contains-any', servlist).get().then(function(snapshot) {
+        if(Object.keys(snapshot).length > 0){
+            snapshot.docs.forEach(function(doc) {            
+                var row = table.insertRow(-1);
+                var cells = [];
+                for (i = 0; i < fields.length; i++){
+                    cells.push(row.insertCell());
+                    cells[i].innerHTML = doc.data()[fields[i]]
+                    cells[i].id = prefix + '-' + fields;
+                }
+            });
+        } else {
+            var error = document.createElement('h1');
+            var error_t = document.createTextNode("No users match your current requirements, try changing them in accounts!")
+            error.appendChild(error_t)
+            document.body.appendChild(error)
+        }
     });
-    })
-    .catch(function(error) {
-        console.log("Error getting document:", error)
-    });  
 }
+
+
+
+
+//
+function query(qtype, utype, user, prefix, fields){
+    var servlist = ['accounting', 'webdev', 'phone', 'legal', 'advertising', 'consulting'];
+    if (user != null)  {   // USER LOGGED IN
+        uid = user.uid;
+        name = user.displayName;
+        email = user.email;
+
+        db.collection(utype).doc(uid).get().then(function(doc) {
+            if (doc != undefined){
+                servlist = doc.data()['services'];
+            } else{
+                var error = document.createElement('h1');
+                var error_t = document.createTextNode("No users match your current requirements, try changing them in accounts!")
+                error.appendChild(error_t)
+                document.body.appendChild(error)
+            }
+            printData(qtype, prefix, fields, servlist)
+        });
+
+    } else {
+        printData(qtype, prefix, fields, servlist)
+    }
+}
+
