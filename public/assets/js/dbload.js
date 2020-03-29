c06ce75cb339c34c9cc46dc1fc0f2010810a8184
Ferruolo <Andrew.Ferruolo@users.noreply.github.com>
2020-03-29 13:06:49 -0400

    worked on biz/vol page

--- public/assets/js/dbload.js
+++ public/assets/js/dbload.js
@@ -1,67 +1,52 @@
-function loadSome(col, prefix, keys) {
+function printData(qtype, prefix, fields, servlist){
+    var table = document.getElementById(prefix+'-table')
 
-    var db = firebase.firestore();
-
-    db.collection(col).get().then(function(snapshot) {
-    snapshot.forEach(function(doc) {
-        console.log(doc.data());
-        
+    db.collection(qtype).where('services', 'array-contains-any', servlist).get().then(function(snapshot) {
+        if(Object.keys(snapshot).length > 0){
+            snapshot.docs.forEach(function(doc) {            
+                var row = table.insertRow(-1);
+                var cells = [];
+                for (i = 0; i < fields.length; i++){
+                    cells.push(row.insertCell());
+                    cells[i].innerHTML = doc.data()[fields[i]]
+                    cells[i].id = prefix + '-' + fields;
+                }
+            });
+        } else {
+            var error = document.createElement('h1');
+            var error_t = document.createTextNode("No users match your current requirements, try changing them in accounts!")
+            error.appendChild(error_t)
+            document.body.appendChild(error)
+        }
     });
-    })
-    .catch(function(error) {
-        console.log("Error getting document:", error)
-    });  
 }
 
 
-function printData(data, prefix, fields){
-    var table = document.getElementById(prefix + "-table");
-
-    var row = table.insertRow(-1);
-    var cells = [];
-   for (i = 0; i < fields.length; i++){
-            cells.push(row.insertCell());
-            cells[i].innerHTML = data[fields[i]]
-            cells[i].id = prefix + '-' + fields;
-        }
-}
 
 
+//
 function query(qtype, utype, user, prefix, fields){
-    if (user == null) {
-        var servlist = ['accounting', 'webdev', 'phone', 'legal', 'advertising', 'consulting'];
-        db.collection("volunteers").where('services', 'array-contains-any', servlist).get().then(
-            snapshot => {
-        snapshot.docs.forEach(doc =>{
-                        
-            printData(doc.data(), prefix, fields)
+    var servlist = ['accounting', 'webdev', 'phone', 'legal', 'advertising', 'consulting'];
+    if (user != null)  {   // USER LOGGED IN
+        uid = user.uid;
+        name = user.displayName;
+        email = user.email;
 
-        }
-        )
-    })
-    } else{
-
-    uid = user.uid;
-    name = user.displayName;
-    email = user.email;
-
-    var userData;
-    db.collection("businesses").doc(uid).get().then(function(doc){
-
-        userData = doc.data();
-
-        db.collection("volunteers").where('services', 'array-contains-any', userData['services']).get().then(snapshot => {
-        snapshot.docs.forEach(doc =>{
-            
-            printData(doc.data(), prefix, fields)
-
+        db.collection(utype).doc(uid).get().then(function(doc) {
+            if (doc != undefined){
+                servlist = doc.data()['services'];
+                console.log(servlist)
+            } else{
+                var error = document.createElement('h1');
+                var error_t = document.createTextNode("No users match your current requirements, try changing them in accounts!")
+                error.appendChild(error_t)
+                document.body.appendChild(error)
+            }
+            printData(qtype, prefix, fields, servlist)
         });
-        
-        
-    })
 
-
+    } else {
+        printData(qtype, prefix, fields, servlist)
+    }
 }
-
-
 
