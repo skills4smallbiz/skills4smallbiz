acee00f40a34a97de429e7762b12fd632ff3cee8
Ferruolo <Andrew.Ferruolo@users.noreply.github.com>
2020-03-31 14:15:00 -0400

    fixed issues with sort

--- public/assets/js/dbload.js
+++ public/assets/js/dbload.js
@@ -14,62 +14,86 @@ function errorMsg() {
     error.style.display = ""
 }
 
-function printData(qtype, prefix, fields, servlist, uid){
+function printData(items, prefix, fields, uid, qtype){
+
+    var numEntries = 0;
     var table = document.getElementById(prefix+'-table')
+    console.log(items)
+    items.forEach(function(doc) { 
+        if (doc.id != uid) {
+            numEntries += 1;
+            var row = table.insertRow(-1);
+            var cells = [];
+            for (i = 0; i < fields.length; i++){
+                cells.push(row.insertCell());
+                var fmtData = doc.data()[fields[i]]
+                if (fields[i] == 'services') {
+                    fmtData = formatServices(fmtData);
+                }
+                cells[i].innerHTML = fmtData
+                cells[i].className = prefix + '-' + fields[i];
+
+                row.addEventListener("click", () => {
+                    window.location.href = "profile.html?type=" + qtype + "&user=" + doc.id;
+                });
+            }
+        }
+    });
+    if (numEntries == 0) {
+        errorMsg();
+    }
+}
+function dist(i, geolocation){
+    var lat = i.data()['lat']
+    var long = i.data()['lng']
+    var dist = Math.sqrt(Math.pow((geolocation['lat']-lat),2) + Math.pow((geolocation['lng']-long), 2)) 
+    console.log(i.data()['bizname'], dist)
+    return dist
+}
+
+
+
+
+
+async function sortData(qtype, prefix, fields, servlist, uid){
 
 
     if (servlist.length > 0) {
         db.collection(qtype).where('services', 'array-contains-any', servlist).get().then(function(snapshot) {
-            var numEntries = 0;
-            orderedItems = []
-            
-            snapshot.docs.forEach((doc)=> orderedItems.push(doc))
+            var items = []
+            printed = false
+            snapshot.docs.forEach((doc)=> items.push(doc))
+
+
+
+
+
             if (prefix == 'biz' && navigator.geolocation){ 
-                    navigator.geolocation.getCurrentPosition(function(position) {
-                    var geolocation = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                        };
-            console.log('My Lat:' + geolocation.lat)
-            console.log('My Long:' + geolocation.lng)
-            orderedItems.sort((i)=>{
-                var lat = i.data()['lat']
-                var long = i.data()['lng']
-                var dist = Math.sqrt(Math.pow((geolocation['lat']-lat),2) + Math.pow((geolocation['lng']-long), 2)) 
-                console.log(i.data()['bizname'], dist) 
+                printData(items, prefix, fields, uid, qtype)
 
-                return dist
+
+
+                navigator.geolocation.getCurrentPosition(function(position) {
+                document.getElementById(prefix +'-table').innerHTML = ""
+                console.log('Not bad')
+                var geolocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                console.log(geolocation)
+                console.log('My Lat:' + geolocation.lat)
+                console.log('My Long:' + geolocation.lng)
+                items.sort(function(a, b){
+                    return dist(a, geolocation) - dist(b, geolocation)
                 })
-            
+                console.log(items)
+                printData(items, prefix, fields, uid, qtype)
+                printed = true;
             })
-        }
-        orderedItems.forEach((x)=>{
-            console.log(x.id, x.data())
-        }
-        ) 
-            orderedItems.forEach(function(doc) { 
-                if (doc.id != uid) {
-                    numEntries += 1;
-                    var row = table.insertRow(-1);
-                    var cells = [];
-                    for (i = 0; i < fields.length; i++){
-                        cells.push(row.insertCell());
-                        var fmtData = doc.data()[fields[i]]
-                        if (fields[i] == 'services') {
-                            fmtData = formatServices(fmtData);
-                        }
-                        cells[i].innerHTML = fmtData
-                        cells[i].className = prefix + '-' + fields[i];
-
-                        row.addEventListener("click", () => {
-                            window.location.href = "profile.html?type=" + qtype + "&user=" + doc.id;
-                        });
-                    }
-                }
-            });
             
-            if (numEntries == 0) {
-                errorMsg();
+                
+            } else{
+                printData(items, prefix, fields, uid, qtype)
             }
         });
     } else {
@@ -97,10 +121,10 @@ function query(qtype, utype, user, prefix, fields){
                 error.appendChild(error_t)
                 document.body.appendChild(error)
             }
-            printData(qtype, prefix, fields, servlist, uid)
+            sortData(qtype, prefix, fields, servlist, uid)
         });
 
     } else {
-        printData(qtype, prefix, fields, servlist)
+        sortData(qtype, prefix, fields, servlist)
     }
 }
