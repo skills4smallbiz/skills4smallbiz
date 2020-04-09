5ed241f91b1839b7d8d5f7463d6d700c1747d82d
Lucien Gaitskell <lucieng@gaitskell.com>
2020-04-09 19:49:30 -0400

    Add replace with dev credentials

--- public/assets/js/setup.js
+++ public/assets/js/setup.js
@@ -4,17 +4,27 @@
 
 
 //function init() {
-    var firebaseConfig = {
-        apiKey: "AIzaSyD-_Zm5y-K8xINw3m9eDeHBnAE4DqtSv50",
-        authDomain: "covidsmallbizlink.firebaseapp.com",
-        databaseURL: "https://covidsmallbizlink.firebaseio.com",
-        projectId: "covidsmallbizlink",
-        storageBucket: "covidsmallbizlink.appspot.com",
-        appId: "1:131149431483:web:20f64b67ec10c9c53c1f9e",
-    };
+var firebaseConfigProd = {
+    apiKey: "AIzaSyD-_Zm5y-K8xINw3m9eDeHBnAE4DqtSv50",
+    authDomain: "covidsmallbizlink.firebaseapp.com",
+    databaseURL: "https://covidsmallbizlink.firebaseio.com",
+    projectId: "covidsmallbizlink",
+    storageBucket: "covidsmallbizlink.appspot.com",
+    appId: "1:131149431483:web:20f64b67ec10c9c53c1f9e",
+};
+
+var firebaseConfigDev = {
+    apiKey: "AIzaSyB7eh9eAW5_MC8kmY3eXt2Rm5f59uWGaAI",
+    authDomain: "covidsmallbizlink-staging.firebaseapp.com",
+    databaseURL: "https://covidsmallbizlink-staging.firebaseio.com",
+    projectId: "covidsmallbizlink-staging",
+    storageBucket: "covidsmallbizlink-staging.appspot.com",
+    messagingSenderId: "857300331152",
+    appId: "1:857300331152:web:516b63439b734c9d9c0615"
+};
 
     // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfigDev);
 var db = firebase.firestore()
 /*
 firebase.database.enableLogging(function (message) {
