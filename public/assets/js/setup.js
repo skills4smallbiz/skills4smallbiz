64821b630ad11e933a5d39ee125f65f0dcd8f7e4
Andrew Ferruolo <andrew.ferruolo@gmail.com>
2020-04-10 13:49:51 -0400

    Created loadprofile.js

--- public/assets/js/setup.js
+++ public/assets/js/setup.js
@@ -1,25 +1,28 @@
-//Initialize by calling below fxn, so we dont need to do so much copy paste
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD-_Zm5y-K8xINw3m9eDeHBnAE4DqtSv50",
+    authDomain: "covidsmallbizlink.firebaseapp.com",
+    databaseURL: "https://covidsmallbizlink.firebaseio.com",
+    projectId: "covidsmallbizlink",
+    storageBucket: "covidsmallbizlink.appspot.com",
+    appId: "1:131149431483:web:20f64b67ec10c9c53c1f9e",
+};
 
-
-
-
-//function init() {
-    var firebaseConfig = {
-        apiKey: "AIzaSyD-_Zm5y-K8xINw3m9eDeHBnAE4DqtSv50",
-        authDomain: "covidsmallbizlink.firebaseapp.com",
-        databaseURL: "https://covidsmallbizlink.firebaseio.com",
-        projectId: "covidsmallbizlink",
-        storageBucket: "covidsmallbizlink.appspot.com",
-        appId: "1:131149431483:web:20f64b67ec10c9c53c1f9e",
-    };
-
-    // Initialize Firebase
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore()
-/*
-firebase.database.enableLogging(function (message) {
-    console.log("[FIREBASE]", message);
-});
-//
 
-*/
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user != null) {
+        // Name, email address etc
+        var name = user.displayName;
+        var email = user.email;
+        console.log("yes");
+        console.log(name);
+
+        b = document.getElementById("sign-btn");
+        b.style.display = "none";
+
+        document.getElementById("account-welcome").innerText = "Hello, " + name;
+        document.getElementById("account-drop").style.display = "";
+    }
+});
